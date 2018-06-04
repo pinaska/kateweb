a56c85178ed0146cf4a9cabdb4691c6ba2acc295
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-06-04 16:30:40 -0700

    code the portfolio archive and single php and front-end

---
+++ themes/kateweb/archive-portfolioitem.php
@@ -0,0 +1,59 @@
+
+<?php
+/**
+ * The main template file.
+ *
+ * @package kateweb_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+				<header>
+					<h1 class="portfolio-item-archive">All our projects are here</h1>
+				</header>
+
+				<div class="portfolio-item-grid container">
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+                <div class="portfolio-grid-item">
+						<div class="portfolio-item-thumbnail">
+							<a href=<?php echo get_post_permalink() ?>><?php the_post_thumbnail( 'large' ); ?></a>
+                        </div>
+
+              <div class="portfolio-item-info">
+                <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                <a class="portfolio-item-button" href=<?php echo get_permalink() ?>>see more</a>
+              </div>
+					</div>
+                <?php endwhile; ?>
+                </div><!--end of portfolio grid-->	
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+    </div><!-- #primary -->
+
+
+    <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<fieldset>
+		<label>
+			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+		</label>
+		<button class="search-submit">
+			<span class="icon-search" aria-hidden="true">
+				<i class="fa fa-search"></i>
+			</span>
+			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
+		</button>
+	</fieldset>
+</form>
+<?php get_footer(); ?>
--- themes/kateweb/single-portfolioitem.php
+++ themes/kateweb/single-portfolioitem.php
@@ -10,29 +10,41 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php while ( have_posts() ) : the_post(); ?>
+
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
-
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<div class="entry-meta">
-			<?php kateweb_posted_on(); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
-<div class="entry-content">
+	<div class="entry-content">
 		<?php the_content(); ?>
-
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+				'after'  => '</div>',
+			) );
+		?>
 		<img src="<?php echo CFS()->get( 'portfolio_item_image' ); ?>" alt="the portfolio item img">
 		<?php echo CFS()->get( 'portfolio_item_description' ); ?>
-		<?php echo CFS()->get( 'portfolio_item_dates' ); ?>
+		<?php echo CFS()->get( 'portfolio_item_start' ); ?>
+		<?php echo CFS()->get( 'portfolio_item_end' ); ?>
 		<?php echo CFS()->get( 'portfolio_item_link' ); ?>
+
+			<?php the_post_navigation(); ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php kateweb_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
+
+
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php wp_footer(); ?>
