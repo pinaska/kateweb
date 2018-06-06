90e3da6b6a767c988cd9a25603f19dbc099cd510
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-06-06 10:09:38 -0700

    get rid of search form from archive-portfolio for now

--- themes/kateweb/archive-portfolio-item.php
+++ themes/kateweb/archive-portfolio-item.php
@@ -19,8 +19,6 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-
-
 				<div class="portfolio-items-grid container">
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
@@ -28,37 +26,22 @@ get_header(); ?>
 						<div class="portfolio-item-thumbnail">
 							<a href=<?php echo get_post_permalink() ?>><?php the_post_thumbnail( 'large' ); ?></a>
 				</div>
-				echo
 
               <div class="portfolio-item-info">
                 <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
                 <a class="portfolio-item-button" href=<?php echo get_permalink() ?>>see more</a>
               </div>
 					</div>
-                <?php endwhile; ?>
-                </div><!--end of portfolio grid-->	
-
+				<?php endwhile; ?>
+				<?php the_posts_navigation(); ?>
+				</div><!--end of portfolio grid-->
+				
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-    </div><!-- #primary -->
-
-
-    <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<fieldset>
-		<label>
-			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
-		</label>
-		<button class="search-submit">
-			<span class="icon-search" aria-hidden="true">
-				<i class="fa fa-search"></i>
-			</span>
-			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
-		</button>
-	</fieldset>
-</form>
+		</main>
+    </div>
 <?php get_footer(); ?>
