7e76850cbb628150b3d110f3fefe3a2e24f2d541
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-07-04 14:32:03 -0700

    coded the loop for front-page displaying portfolio-items. next: get tags

--- themes/kateweb/front-page.php
+++ themes/kateweb/front-page.php
@@ -12,25 +12,48 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<section class="hero-divs">
+						<!-- <div class="entry-content"> -->
+								<div class="front-page-container">
+									<div class="front-page-item">
+										<?php echo CFS()->get('first_section')?>
+									</div>
+									<div class="front-page-item">
+									<?php echo CFS()->get('second_section')?>
+									</div>
+									<div class="front-page-item">
+										<!-- <img class="position-letter" src="assets/p.svg"> -->
+										<?php echo CFS()->get('third_section')?>
+									</div>
+								</div><!--.front-page-container-->
+						<!-- </div>.entry-content -->
 
-                    <!-- <div class="entry-content"> -->
-							<div class="front-page-container">
-								<div class="front-page-item">
-									<?php echo CFS()->get('first_section')?>
-								</div>
-								<div class="front-page-item">
-								<?php echo CFS()->get('second_section')?>
-								</div>
-								<div class="front-page-item">
-									<!-- <img class="position-letter" src="assets/p.svg"> -->
-									<?php echo CFS()->get('third_section')?>
-								</div>
-							</div><!--.front-page-container-->
-                    <!-- </div>.entry-content -->
-                </article><!-- #post-## -->
+				</section>
+				<section class="portfolio-display">
+						<?php
+						$args = array(
+							'post_type' => 'portfolio-item',
+							'posts_per_page' => 4,
+							'orderby' => 'date',
+							'order' => 'DESC'
+							);
 
+						$portfolio_items = get_posts( $args );?>
 
+						<?php foreach ( $portfolio_items as $post ) : setup_postdata( $post );?>
+						<div class="portfolio-grid-item">
+						<div class="portfolio-item-thumbnail">
+							<a href=<?php echo get_post_permalink() ?>><?php the_post_thumbnail( 'thumbnail' ); ?></a>
+				</div>
+						<div class="portfolio-item-info">
+                <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                <a class="portfolio-item-button" href=<?php echo get_permalink() ?>>see more</a>
+              </div>
+
+						<?php endforeach; wp_reset_postdata(); ?>
+				</section>
+				</article><!-- #post-## -->
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
